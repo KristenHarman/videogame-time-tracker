94b62fbe46d945d6fc7710b5b43de087297559d3
LeoZai613 <102532972+LeoZai613@users.noreply.github.com>
2022-09-03 16:14:25 -0400

    got dec to work

--- public/js/main.js
+++ public/js/main.js
@@ -78,22 +78,21 @@ async function markIncomplete(){
     }catch(err){
         console.log(err)
     }
-
-    async function decHealth(){
-        const gameId = this.parentNode.dataset.id
-        try{
-            const response = await fetch('games/markIncomplete', {
-                method: 'put',
-                headers: {'Content-type': 'application/json'},
-                body: JSON.stringify({
-                    'gameIdFromJSFile': gameId
-                })
-            })
-            const data = await response.json()
-            console.log(data)
-            location.reload()
-        }catch(err){
-            console.log(err)
-        }
 }
+async function decHealth(){
+    const gameId = this.parentNode.dataset.id
+    try{
+        const response = await fetch('games/markIncomplete', {
+            method: 'put',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                'gameIdFromJSFile': gameId
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
 }
